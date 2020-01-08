71316ebd09e31980f62e7d25249e440cf182ae50
Ives <ivesmoreira3@gmail.com>
2020-01-08 22:41:58 +0000

    new CRUD, complete logic

---
+++ src/components/App.js
@@ -0,0 +1,100 @@
+import React,{ Component } from 'react';
+import { Container } from './styles.js';
+
+export default class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      title: 'Notes System',
+      titles: [],
+      annotations: [],
+      verify: 0,
+      index: '',
+      datas: [],
+    }
+  } 
+
+  fSubmit = (e) =>{
+    e.preventDefault();
+
+    let datas = this.state.datas;
+    let name = this.refs.name.value;
+    let address = this.refs.address.value;
+
+    this.setState({ titles: {...this.state.titles, name} });
+    this.setState({ annotations: {...this.state.annotations, address} });
+
+    if(this.state.verify === 0) {
+      let data = {
+        name, address
+      }
+
+      datas.push(data);
+    }
+    else {
+      let index = this.state.index;
+      datas[index].name = name;
+      datas[index].address = address;
+    }   
+    
+    this.setState({
+      datas: datas,
+      verify: 0,
+    });
+
+    this.refs.myForm.reset();
+  }
+
+  fRemove = (i) => {
+    let datas = this.state.datas;
+    datas.splice(i,1);
+    this.setState({
+      datas: datas
+    });
+
+    this.refs.myForm.reset();
+  }
+
+  fEdit = (i) => {
+    let data = this.state.datas[i];
+    this.refs.name.value = data.name;
+    this.refs.address.value = data.address;
+
+    this.setState({
+      verify: 1,
+      index: i,
+    });
+  }  
+
+  render() {
+    let datas = this.state.datas;
+    return (
+      <div className="App">
+        <div>
+          <h2>{this.state.title}</h2>
+        </div>
+        <Container>
+          <form ref="myForm" className="myForm">
+            <input maxLength="27" type="text" ref="name" placeholder="Title" className="formField" />
+            <textarea type="text" ref="address" placeholder="Annotation" className="formField" />
+            <button onClick={(e)=>this.fSubmit(e)} className="myButton">ADD</button>
+          </form>
+
+          <pre>
+            {datas.map((data, i) =>  
+              <div key={i} className="myList">
+                <p>Title:</p> 
+                <label> {data.name} </label> <br /> 
+                <p>Annotation:</p> 
+                <textarea readOnly className="annotationText" value={data.address} /> <br />
+                <button onClick={()=>this.fRemove(i)} className="myListButton">DELETE</button>
+                <button onClick={()=>this.fEdit(i)} className="myListButton1">EDIT</button>
+              </div>                   
+            )}
+          </pre>
+        </Container>        
+      </div>
+    );
+  }
+}
---
+++ src/components/styles.js
@@ -0,0 +1,128 @@
+import styled from 'styled-components';
+
+export const Container = styled.div `
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+  grid-template-rows: 2fr 2fr;
+  justify-items: center;
+
+  .myForm {
+    width: 100%;
+    max-width: 300px;
+    min-width: 300px;
+    background: white;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-radius: 3px;
+    height: 200px;
+    box-shadow: 1px 1px 20px #000;
+    margin: 0 auto;
+    margin-bottom: 40px;
+    }
+    input {
+      width: 250px;
+      border: 1px solid #000;
+      height: 40px;
+      font-size: 15px;
+      font-family: Verdana, Geneva, Tahoma, sans-serif;
+      outline: none;
+      color: #2E0854;
+      padding-left: 5px;
+      border-radius: 3px;
+      margin-top: 20px;
+      margin-bottom: 20px;
+    }
+    textarea {
+      width: 250px;
+      border: 1px solid #000;
+      height: 100px;
+      font-size: 15px;
+      font-family: Verdana, Geneva, Tahoma, sans-serif;
+      outline: none;
+      color: #2E0854;
+      padding-left: 5px;
+      border-radius: 3px;
+      resize: none;
+    }
+    .myButton {
+      width: 80px;
+      height: 35px;
+      margin-top: 10px;
+      margin-bottom: 10px;
+      margin-left: 180px;
+      border: none;
+      border-radius: 3px;
+      outline: none;
+      font-size: 20px;
+      font-family: Verdana, Geneva, Tahoma, sans-serif;
+      color: white;
+      background: #8861E8;
+      font-weight: bold;
+      box-shadow: 2px 2px 5px #000;
+    }
+
+    .myList {
+      width: 100%;
+      max-width: 300px;
+      min-width: 300px;
+      background: white;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      border-radius: 3px;
+      height: 300px;
+      box-shadow: 1px 1px 20px #000;
+      margin: 0 auto;
+      margin-bottom: 40px;
+
+      p {
+        font-size: 15px;
+        color: #8861E8;
+      }
+
+      label {
+        color: #2E0854;
+        font-size: 15px;
+      }
+
+      .annotationText {
+        height: 100px;
+        border: none;
+        scrollbar-width: 2px;
+      } 
+
+      button:nth-child(odd) {
+        background-color: red;
+        width: 100px;
+        height: 25px;
+        border: none;
+        border-radius: 3px;
+        outline: none;
+        font-size: 20px;
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        color: white;
+        font-weight: bold;
+        box-shadow: 2px 2px 5px #000;
+        float: left;
+        margin-bottom: 10px;
+        margin-top: 20px;
+      }
+      button:nth-child(even) {
+        background-color: yellow;
+        width: 100px;
+        height: 25px;
+        border: none;
+        border-radius: 3px;
+        outline: none;
+        font-size: 20px;
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        color: white;
+        font-weight: bold;
+        box-shadow: 2px 2px 5px #000;
+        float: left;
+        margin-bottom: 10px;
+       
+    }
+  }`;
